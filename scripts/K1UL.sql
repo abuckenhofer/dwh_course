c2ff2dbbcac5b499aacf550f8b246c18a90c0b00
Andreas Buckenhofer <abuckenhofer@users.noreply.github.com>
2017-11-05 20:05:33 +0100

    Update K1UL.sql

--- scripts/K1UL.sql
+++ scripts/K1UL.sql
@@ -1,1 +1,16 @@
+-- clear tables in Core Warehouse Layer
+
+truncate table S_BOOKING_FLK;
+truncate table S_CUSTOMER_CRM;
+truncate table S_RENTAL_ZONE_FLK;
+truncate table S_VEHICLE_FLK;
+
+truncate table L_BOOKING;
+
+delete from H_BOOKING;
+delete from H_CUSTOMER;
+delete from H_RENTAL_ZONE;
+delete from H_VEHICLE;
+
+delete from R_CATEGORY;
 
