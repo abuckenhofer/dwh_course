b9c06b6526d5fb02d0494e20ace79f3df59e449a
Andreas Buckenhofer <abuckenhofer@users.noreply.github.com>
2017-11-08 07:07:13 +0100

    Create SGN0.sql

---
+++ scripts/SGN0.sql
@@ -0,0 +1,7 @@
+-- truncate staging
+
+Truncate table STG_BOOKING_CARSHARING;
+Truncate table STG_CATEGORY_CARSHARING;
+Truncate table STG_RENTAL_ZONE_CARSHARING;
+Truncate table STG_VEHICLE_CARSHARING;
+Truncate table STG_CUSTOMER;
