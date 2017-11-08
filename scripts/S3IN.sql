dcb82e2863067fb2280cdf6c267a0aa6e0c8456d
Andreas Buckenhofer <abuckenhofer@users.noreply.github.com>
2017-11-08 16:21:20 +0100

    Update S3IN.sql

--- scripts/S3IN.sql
+++ scripts/S3IN.sql
@@ -4,15 +4,15 @@
 
 ---------------------------------   
 -- 01
----------------------------------   
+--------------------------------- 
 WITH lead_cust as (
-   SELECT lead (s.valid_from, 1) OVER (PARTITION BY s.customer_key 
-                      ORDER BY s.valid_from ASC) as end_ts
-         , s.*
-   FROM s_customer s)               
-SELECT * 
-FROM   lead_cust b
-WHERE  b.end_ts IS NULL;
+  SELECT lead (s.valid_from, 1) OVER (PARTITION BY s.customer_key
+                                ORDER BY s.valid_from ASC) as end_ts
+       , s.*
+   FROM s_customer s)
+SELECT *
+FROM  lead_cust b
+WHERE b.end_ts IS NULL;
 ---------------------------------                     
 
 ---------------------------------   
