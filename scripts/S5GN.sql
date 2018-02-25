3cf25bef778c2256bc558c1f231895eaac5a151d
Andreas Buckenhofer <abuckenhofer@users.noreply.github.com>
2018-02-25 17:49:11 +0100

    First name removed from task description

--- scripts/S5GN.sql
+++ scripts/S5GN.sql
@@ -1,11 +1,10 @@
 -- Which 3 customers drove the longest distances?
 
 SELECT c.last_name
-     , c.first_name
      , SUM(b.distance) AS dist
 FROM   stg_booking_carsharing b
 JOIN   stg_customer c ON c.customer_id = b.customer_hal_id
-GROUP BY c.customer_id, c.last_name, c.first_name
+GROUP BY c.customer_id, c.last_name
 ORDER BY sum(b.distance) desc
 FETCH FIRST 3 ROWS ONLY
 ;
@@ -13,11 +12,25 @@ FETCH FIRST 3 ROWS ONLY
 /*
 
 SELECT MAX(c.last_name)
-     , MAX(c.first_name)
      , SUM(b.distance) AS dist
 FROM   stg_booking_carsharing b
 JOIN   stg_customer c ON c.customer_id = b.customer_hal_id
 GROUP BY c.customer_id
+ORDER BY sum(b.distance) desc
+FETCH FIRST 3 ROWS ONLY
+;
+
+*/
+
+/*
+
+WRONG !!!
+
+SELECT c.last_name
+     , SUM(b.distance) AS dist
+FROM   stg_booking_carsharing b
+JOIN   stg_customer c ON c.customer_id = b.customer_hal_id
+GROUP BY c.last_name
 ORDER BY sum(b.distance) desc
 FETCH FIRST 3 ROWS ONLY
 ;
