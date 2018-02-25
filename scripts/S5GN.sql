817ea833e4096a6ea2c193a29bc150ced8b4e668
Andreas Buckenhofer <abuckenhofer@users.noreply.github.com>
2018-02-25 17:45:04 +0100

    Correction group by

--- scripts/S5GN.sql
+++ scripts/S5GN.sql
@@ -5,7 +5,21 @@ SELECT c.last_name
      , SUM(b.distance) AS dist
 FROM   stg_booking_carsharing b
 JOIN   stg_customer c ON c.customer_id = b.customer_hal_id
-GROUP BY c.last_name, c.first_name
+GROUP BY c.customer_id, c.last_name, c.first_name
 ORDER BY sum(b.distance) desc
 FETCH FIRST 3 ROWS ONLY
 ;
+
+/*
+
+SELECT MAX(c.last_name)
+     , MAX(c.first_name)
+     , SUM(b.distance) AS dist
+FROM   stg_booking_carsharing b
+JOIN   stg_customer c ON c.customer_id = b.customer_hal_id
+GROUP BY c.customer_id
+ORDER BY sum(b.distance) desc
+FETCH FIRST 3 ROWS ONLY
+;
+
+*/
